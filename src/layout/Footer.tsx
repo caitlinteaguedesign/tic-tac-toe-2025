7728f46bdddbd1683b74c93b669f64e5538a354c
Caitlin Teague Doerr <design@caitlinteague.com>
2025-09-24 23:17:35 -0500

    modulerize layout

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,9 @@
+function footer() {
+  return (
+    <footer>
+      <h1 className="text-white text-xl">Footer</h1>
+    </footer>
+  );
+}
+
+export default footer;
---
+++ src/layout/Main.tsx
@@ -0,0 +1,9 @@
+function main() {
+  return (
+    <main>
+      <h1 className="text-white text-4xl">A battle of modest proportions.</h1>
+    </main>
+  );
+}
+
+export default main;

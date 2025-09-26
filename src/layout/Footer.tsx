720ca7e0522af0c49091f1ce632f261666077a47
Caitlin Teague Doerr <design@caitlinteague.com>
2025-09-26 10:55:29 -0500

    tweak layout, create link class, add default line-height

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,22 +2,16 @@ function Footer() {
   const date = new Date().getFullYear();
 
   return (
-    <footer className="p-10 pb-16  bg-(--color-neutral-800) text-center grid gap-2">
+    <footer className="p-10 pb-16  bg-(--color-neutral-800) text-center grid gap-4">
       <p>©{date} Caitlin Teague Doerr</p>
       <p>
         To contact owner/developer, send an email to{" "}
-        <a
-          href="mailto:design@caitlinteague.com"
-          className="underline text-(--color-interactive)"
-        >
+        <a href="mailto:design@caitlinteague.com" className="link">
           design@caitlinteague.com
         </a>
       </p>
       <p>
-        <a
-          href="//caitlinteague.com/builds"
-          className="underline text-(--color-interactive)"
-        >
+        <a href="//caitlinteague.com/builds" className="link">
           View more projects in Caitlin's portfolio
         </a>
       </p>

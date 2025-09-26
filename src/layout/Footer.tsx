163d6f41926e8904b057f06dc574c8fa603858ac
Caitlin Teague Doerr <design@caitlinteague.com>
2025-09-25 21:19:31 -0500

    add footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,26 @@
 function Footer() {
+  const date = new Date().getFullYear();
+
   return (
-    <footer className="p-10 pb-16 bg-zinc-700 text-white">
-      <p>Footer</p>
+    <footer className="p-10 pb-16 bg-zinc-700 text-white text-center grid gap-4">
+      <p>©{date} Caitlin Teague Doerr</p>
+      <p>
+        To contact owner/developer, send an email to{" "}
+        <a
+          href="mailto:design@caitlinteague.com"
+          className="underline text-(--color-primary-100)"
+        >
+          design@caitlinteague.com
+        </a>
+      </p>
+      <p>
+        <a
+          href="//caitlinteague.com/builds"
+          className="underline text-(--color-primary-100)"
+        >
+          View more projects in Caitlin's portfolio
+        </a>
+      </p>
     </footer>
   );
 }

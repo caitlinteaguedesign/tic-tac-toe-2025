660cc73964ede02d8c7e8786f89dffbe6396ea5a
Caitlin Teague Doerr <cdoerr@illustrativemathematics.org>
2025-10-03 14:36:55 -0500

    add basic card

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -4,6 +4,10 @@ function Main() {
   return (
     <main className="p-10 pb-14 grow shrink-0 flex flex-col items-center justify-center">
       <Header />
+      <div className="card">test</div>
+      <div className="card">test</div>
+      <div className="card">test</div>
+      <div className="card">test</div>
     </main>
   );
 }

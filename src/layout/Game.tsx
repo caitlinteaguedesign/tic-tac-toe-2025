fb8686c47ca3e9b1c2d837d2982aad4790a8342a
Caitlin Teague Doerr <design@caitlinteague.com>
2025-10-13 12:23:13 -0500

    add classnames package, add Button

--- src/layout/Game.tsx
+++ src/layout/Game.tsx
@@ -1,26 +1,13 @@
+import Button from "../components/Button";
+
 function Game() {
   return (
     <div className="grid grid-cols-3 grid-rows-3 grid-flow-col gap-3">
       <div className="card row-span-3 min-w-[166px]">
         <h2 className="type-interface">Moves</h2>
-        <button
-          type="button"
-          className="button button--primary button--interactive"
-        >
-          <span className="text">Button</span>
-        </button>
-        <button
-          type="button"
-          className="button button--interactive button--secondary"
-        >
-          Button
-        </button>
-        <button
-          type="button"
-          className="button button--secondary button--disabled"
-        >
-          Button
-        </button>
+        <Button label="Label" style="primary" arrow="right" />
+        <Button label="Secondary" style="secondary" />
+        <Button label="Disabled" style="secondary" disabled />
       </div>
       <div className="card row-start-1">Status</div>
       <div className="card row-start-2">Board</div>

c0f3d3e0caa70ecf3fd9f80c9a0aa9850ebe1b7b
Caitlin Teague Doerr <design@caitlinteague.com>
2025-10-13 22:32:29 -0500

    refactor button focus, add maps to history buttons and squares, add history function

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -24,17 +24,14 @@ function Board({ xIsNext, squares, onPlay }: BoardProps) {
 
   return (
     <div className="board">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      {squares.map((_square, i) => (
+        <Square
+          key={`square_${i}`}
+          value={squares[i]}
+          winning={i === 0 || i === 1 || i === 2}
+          onSquareClick={() => handleClick(i)}
+        />
+      ))}
     </div>
   );
 }

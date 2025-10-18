258f2bee0dd0c6253c22d9e7b1317b028d336323
Caitlin Teague Doerr <design@caitlinteague.com>
2025-10-18 18:46:11 -0500

    update format options

--- src/util/formatTime.ts
+++ src/util/formatTime.ts
@@ -1,14 +1,18 @@
-const formatTime = (milliseconds: number) => {
+const formatTime = (milliseconds: number, includeMs: boolean = false) => {
   const totalSeconds = Math.floor(milliseconds / 1000);
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = totalSeconds % 60;
   const ms = Math.floor((milliseconds % 1000) / 10); // Displaying in 10ms increments
 
-  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+  let time = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
     2,
     "0"
-  )}:${String(seconds).padStart(2, "0")}.${String(ms).padStart(2, "0")}`;
+  )}:${String(seconds).padStart(2, "0")}`;
+
+  if (includeMs) time = `${time}.${String(ms).padStart(2, "0")}`;
+
+  return time;
 };
 
 export default formatTime;

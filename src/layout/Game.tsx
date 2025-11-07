c22e69a3ccd4b152b7a97e6bcbf773303d287506
Caitlin Teague Doerr <design@caitlinteague.com>
2025-11-07 17:23:27 -0600

    refactor status component

--- src/layout/Game.tsx
+++ src/layout/Game.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Mark, type TSquare } from "../types/TSquare";
+import type { TSquare } from "../types/TSquare";
+import { GameStateContext } from "../util/GameStateContext";
 import { GameState, type TGameState } from "../types/TGameState";
-import { GameStateContext } from "../util/gameStateContext";
 import calculateTie from "../util/calculateTie";
 import calculateWinner from "../util/calculateWinner";
 import isXNext from "../util/isXNext";
@@ -11,17 +11,20 @@ import History from "../components/History";
 import Status from "../components/Status";
 import StopWatch from "../components/StopWatch";
 
-const INITIAL_STATUS = "Your move player X";
 const INITIAL_HISTORY = [Array(9).fill(null)];
 
 const Game = () => {
   const [history, setHistory] = useState<TSquare[][]>(INITIAL_HISTORY);
   const [currentMove, setCurrentMove] = useState<number>(0);
-  const [status, setStatus] = useState<string>(INITIAL_STATUS);
   const [gameState, setGameState] = useState<TGameState>(GameState.PLAY);
   const [timerKey, setTimerKey] = useState<number>(0);
   const xIsNext = isXNext(currentMove);
   const currentSquares = history[currentMove];
+
+  const gameStateContextValue = {
+    mode: gameState,
+    xIsNext: isXNext(currentMove),
+  };
 
   // todo: this is used by squares to update the history
   const makeMove = (nextSquares: TSquare[]) => {
@@ -30,12 +33,12 @@ const Game = () => {
     setHistory(nextHistory);
     setCurrentMove(nextMove);
     // runs after every move and transversing the history
-    updateGameStatus(nextHistory[nextMove], nextMove);
+    updateGameStatus(nextHistory[nextMove]);
   };
 
   const goToMove = (move: number) => {
     setCurrentMove(move);
-    updateGameStatus(history[move], move);
+    updateGameStatus(history[move]);
   };
 
   const undoMove = () => {
@@ -50,39 +53,25 @@ const Game = () => {
     }
   };
 
-  // todo: maybe the text can go into status block if winners, next player, and game state are
-  // exposed in use context anyway, like the sqaurs will need
-  // this function just needs to validate the current history
-  const updateGameStatus = (squares: TSquare[], move: number) => {
-    setStatus(`Your move player ${isXNext(move) ? Mark.X : Mark.O}`);
-    setGameState(GameState.PLAY);
-
-    const winningSquares = calculateWinner(squares);
-
-    if (winningSquares) {
-      const winnerName = squares[winningSquares[0]];
-      setStatus(
-        `Player ${winnerName}, wouldn't you prefer a good game of chess?`
-      );
+  const updateGameStatus = (squares: TSquare[]) => {
+    if (calculateWinner(squares)) {
       setGameState(GameState.WIN);
+    } else if (calculateTie(squares)) {
+      setGameState(GameState.TIE);
     } else {
-      if (calculateTie(squares)) {
-        setStatus("A strange game. The only winning move is not to play.");
-        setGameState(GameState.TIE);
-      }
+      setGameState(GameState.PLAY);
     }
   };
 
   const resetGame = () => {
     setHistory(INITIAL_HISTORY);
     setCurrentMove(0);
-    setStatus(INITIAL_STATUS);
     setGameState(GameState.PLAY);
     setTimerKey((prevKey) => prevKey + 1);
   };
 
   return (
-    <GameStateContext value={gameState}>
+    <GameStateContext value={gameStateContextValue}>
       <div className="game-layout">
         <Controls
           disableUndo={currentMove <= 1}

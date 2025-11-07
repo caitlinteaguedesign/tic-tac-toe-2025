4e36e0353d7cf50635d34a7ce1d6159527fd2c2f
Caitlin Teague Doerr <design@caitlinteague.com>
2025-11-07 17:45:50 -0600

    save progress

--- src/util/GameStateContext.ts
+++ src/util/GameStateContext.ts
@@ -4,9 +4,11 @@ import { GameState, type TGameState } from "../types/TGameState";
 type GameStateContextProps = {
   mode: TGameState;
   xIsNext: boolean;
+  winnerIds: number[];
 };
 
 export const GameStateContext = createContext<GameStateContextProps>({
   mode: GameState.PLAY,
   xIsNext: true,
+  winnerIds: [-1, -1, -1],
 });

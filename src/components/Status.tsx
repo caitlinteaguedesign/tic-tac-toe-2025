ba096ecc866c5e9f32e137ae61ddb1f0dc23cf24
Caitlin Teague Doerr <design@caitlinteague.com>
2025-11-07 17:23:27 -0600

    refactor status component

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,27 +1,30 @@
 import { useContext } from "react";
 import classNames from "classnames";
 import { GameState } from "../types/TGameState";
-import { GameStateContext } from "../util/gameStateContext";
+import { Mark } from "../types/TSquare";
+import { GameStateContext } from "../util/GameStateContext";
 
 const Status = () => {
-  const gameMode = useContext(GameStateContext);
+  const { mode, xIsNext } = useContext(GameStateContext);
 
   const cardClasses = classNames("card status-layout status-card", {
-    tie: gameMode === GameState.TIE,
-    winner: gameMode === GameState.WIN,
+    tie: mode === GameState.TIE,
+    winner: mode === GameState.WIN,
   });
 
   let statusMessage = "Your move player X";
 
-  switch (gameMode) {
+  switch (mode) {
     case GameState.PLAY:
-      statusMessage = "Your move player [?]";
+      statusMessage = `Your move player ${xIsNext ? Mark.X : Mark.O}`;
       break;
     case GameState.TIE:
       statusMessage = "A strange game. The only winning move is not to play.";
       break;
     case GameState.WIN:
-      statusMessage = "Player [?], wouldn't you prefer a good game of chess?";
+      statusMessage = `Player ${
+        xIsNext ? Mark.O : Mark.X
+      }, wouldn't you prefer a good game of chess?`;
       break;
     default:
       break;

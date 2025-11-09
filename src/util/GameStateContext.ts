d3f779dc0f29bee3255b7f2631829e1df8eebdeb
Caitlin Teague Doerr <design@caitlinteague.com>
2025-11-08 22:31:22 -0600

    refactor square, board, context, status, add inital const file

--- src/util/GameStateContext.ts
+++ src/util/GameStateContext.ts
@@ -1,14 +1,20 @@
 import { createContext } from "react";
 import { GameState, type TGameState } from "../types/TGameState";
+import { INITIAL_HISTORY, INITIAL_WINNER_IDS } from "./initial";
+import type { TSquare } from "../types/TSquare";
 
 type GameStateContextProps = {
   mode: TGameState;
   xIsNext: boolean;
+  current: TSquare[];
   winnerIds: number[];
+  onMove: (nextSquares: TSquare[]) => void;
 };
 
 export const GameStateContext = createContext<GameStateContextProps>({
   mode: GameState.PLAY,
   xIsNext: true,
-  winnerIds: [-1, -1, -1],
+  current: INITIAL_HISTORY[0],
+  winnerIds: INITIAL_WINNER_IDS,
+  onMove: () => {},
 });
---
+++ src/util/initial.ts
@@ -0,0 +1,2 @@
+export const INITIAL_HISTORY = [Array(9).fill(null)];
+export const INITIAL_WINNER_IDS = [-1, -1, -1];

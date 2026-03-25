8c7a2f12ce4dfad1783e44d878496d103c556814
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-24 19:20:42 -0500

    update footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,13 +2,14 @@ const Footer = () => {
   const date = new Date().getFullYear();
 
   return (
-    <footer className="p-10 pb-16  bg-(--color-neutral-800) text-center grid gap-4">
+    <footer className="p-8 pb-16 bg-(--color-neutral-800) text-center grid gap-4">
       <p>©{date} Caitlin Teague Doerr</p>
       <p>
-        To contact owner/developer, send an email to{" "}
+        To contact owner/developer,{" "}
         <a href="mailto:design@caitlinteague.com" className="link">
-          design@caitlinteague.com
-        </a>
+          send an email
+        </a>{" "}
+        to <span className="font-bold">design@caitlinteague.com</span>
       </p>
       <p>
         <a href="//caitlinteague.com/builds" className="link">
